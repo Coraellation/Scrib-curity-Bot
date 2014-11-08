89e9f3eee2a6546529f937ba94427cd95902e1e7
DeonHua <deonhbci@gmail.com>
2014-11-07 20:32:14 -0500

    Added documentation.

--- Scrib-curity.py
+++ Scrib-curity.py
@@ -1,19 +1,24 @@
 from myro import *
 import Security
 
+#The security system is a Global for now.
 security = Security.Security()
 
 
 def activated(self):
+    '''This runs while the robot is activated.
+    It checks to see if the user wants to deactivate the system.
+    '''
     global security
     #insert sensor checking for objects
     speak("Security is now activated, to deactivate, cover the left light sensor.")
 
-    #If all sensors are covered, go to password entry.
+    #If the left sensor is covered, go to password entry.
     while (1):
         wait(0.5)
         print getLight()
         if getLight ("left") > security.getLeftS():
+            #If it's not locked out, it allows for deactivation
             if not self.__lockedOut:
                 if security.checkPassword():
                     deactivated()
@@ -26,6 +31,10 @@ def activated(self):
 
 
 def deactivated(self):
+    '''This runs while the robot is deactivated.
+    Checks to see if the user wants to activate the system
+    or change the password.
+    '''
     global security
 
     speak("The robot is ready to be activated.", 0)
@@ -34,9 +43,11 @@ def deactivated(self):
 
     while(1):
         wait(0.5)
+        #If the left sensor is covered, it activates
         if getLight("left") > security.getLeftS():
             activated()
 
+        #If the center sensor is covered, you can change your password
         if getLight("center") > security.getCenterS():
             if security.checkPassword():
                 self.__password = security.setPassword()
@@ -46,6 +57,8 @@ def deactivated(self):
 
 
 def main():
+    '''Main loop. Starts the program off.
+    '''
     global security
     init("COM3")
     security.setPassword()
