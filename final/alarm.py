47d67e9412d6f71e450f9fddc54f337c2c6c030c
Dian Jin <gryffindorstudent@gmail.com>
2014-11-16 17:34:46 -0500

    added more comments lol

--- final/alarm.py
+++ final/alarm.py
@@ -19,6 +19,8 @@ i=0
 f = 0.1
 alarmtime = 50
 
+## placeObjects is the function that prompts the user to put their objects in front of the screen.
+## this function is called only once.
 def placeObjects():
     ## Place your objects
     speak("Place your objects now.")
@@ -30,6 +32,8 @@ def placeObjects():
     global black0
     black0 = countBlack(picture)
 
+## setBounds is the function that defines xlow, xhigh, ylow, and yhigh, values that are used to count black pixels later on. 
+## this function is called only once. 
 def setBounds():
     configureBlob(200, 255, 100, 150, 100, 150)
     picture = takePicture("blob")
@@ -45,6 +49,8 @@ def setBounds():
     area = xdiff * ydiff
     print("iteration space: " + str(area))
 
+## this function returns true if a given pixel is black.
+## this function is called multiple times by countBlack.
 def isBlack(pixel):
     rgb = pixel.getRGB()[0]
     if rgb==255:
@@ -54,6 +60,8 @@ def isBlack(pixel):
     else:
         return True
 
+## this function returns the number of black pixels in a given picture.
+## this function is called in the alarmIterate() function.
 def countBlack(pic):
     counter = 0;
     for x in range(xlow+frametol, xhigh-frametol):
@@ -62,6 +70,7 @@ def countBlack(pic):
                 counter+=1
     return counter;
 
+## this function is used to find xlow and xhigh. It is called by setBounds() once.
 def xBounds(pic):
     ## find x-bounds
     for x_lower in range (0, 1280):
@@ -69,8 +78,8 @@ def xBounds(pic):
             for x_upper in range (x_lower, 1280):
                 if (isBlack(getPixel(pic,x_upper,400)) == True):
                     return [x_lower, x_upper, x_upper-x_lower]
-    return "YOU FUCKED UP"
 
+## this function is used to find ylow and yhigh. It is called by setBounds() once.
 def yBounds(pic):
     ## find y-bounds
     for y_lower in range (0, 800):
@@ -79,7 +88,8 @@ def yBounds(pic):
                 if (isBlack(getPixel(pic,640,y_upper)) == True):
                     return [y_lower, y_upper, y_upper-y_lower]
 
-
+## this function takes pictures, counts the black pixels, and sounds the alarm if an object is removed.
+## this function is meant to be called in a while loop.
 def alarmIterate():
     global i
     picture = takePicture("blob")
@@ -96,6 +106,7 @@ def alarmIterate():
     black0 = black1-30
     i+=1
 
+## this function just sounds an alarm lol. it is meant to be called in a for loop so it beeps a given number of times.
 def alarmSound():
     beep(f, 2903, 1975.53)
     beep(f, 2093, 1864.66)
